142e9e57f987f1d9384ead7e9fc70de37a16a865
Matheus Rosendo Pedreira <matrpedreira@gmail.com>
2013-02-16 19:44:39 -0200

    Criação do Projeto JAVA
    Criação do proj, biblioteca SQL já adicionada

---
+++ Proj_Copa/src/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.sql.*;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
